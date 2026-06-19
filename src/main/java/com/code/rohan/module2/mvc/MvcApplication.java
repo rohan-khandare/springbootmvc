47ba2567b38d11d1d7eee83273f2229a052f4900
Rohan Khandare <rohankhandare429@gmail.com>
2026-06-19 16:47:16 +0530

    adding all required files

---
+++ src/main/java/com/code/rohan/module2/mvc/MvcApplication.java
@@ -0,0 +1,13 @@
+package com.code.rohan.module2.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/rohan/module2/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.rohan.module2.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
